177774d736184cc11c0b19773d55aab61dd351e1
gamingnut1 <brettdavault@gmail.com>
2015-05-17 11:21:30 -0500

    Create Refrence.java
    Reference

---
+++ Refrence.java
@@ -0,0 +1,9 @@
+package com.kidokenji.mineartonline;
+
+public class Refrence {
+	public static final String MOD_ID = "mao";
+	public static final String MOD_NAME = "MineArtOnline Mod";
+	public static final String Version = "0.0.2";
+	public static final String CLIENT_PROXY_CLASS = "com.kidokenji.mineartonline.proxy.ClientProxy";
+	public static final String SERVER_PROXY_CLASS = "com.kidokenji.mineartonline.proxy.CommonProxy";
+}
