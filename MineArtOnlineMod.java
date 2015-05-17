5b20a2a7b282e34e832d25d3b1b1d4b5fb3d2372
gamingnut1 <brettdavault@gmail.com>
2015-05-17 11:24:49 -0500

    Update MineArtOnlineMod.java

--- MineArtOnlineMod.java
+++ MineArtOnlineMod.java
@@ -1,3 +1,29 @@
+package com.kidokenji.mineartonline;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+import com.kidokenji.mineartonline.init.MAOBlocks;
+import com.kidokenji.mineartonline.init.MAOItems;
+import com.kidokenji.mineartonline.proxy.CommonProxy;
+
+@Mod(modid = Refrence.MOD_ID, name = Refrence.MOD_NAME, version = Refrence.Version)
+public class MineArtOnlineMod {
+	
+	@SidedProxy(clientSide = Refrence.CLIENT_PROXY_CLASS, serverSide = Refrence.SERVER_PROXY_CLASS)
+	public static CommonProxy proxy;
+	
+	public static final MaoTab tabmao = new MaoTab("tabmao");
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		
+	}
+	
 	
 	{
 		MAOBlocks.init();
