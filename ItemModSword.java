57bd2f9e6ee38f55048c9f67e6b8938292ea9aa4
gamingnut1 <brettdavault@gmail.com>
2015-05-17 11:16:08 -0500

    Create ItemModSword.java
    The file to call for sword vairables

---
+++ ItemModSword.java
@@ -0,0 +1,12 @@
+package com.kidokenji.mineartonline.Items;
+
+import net.minecraft.item.ItemSword;
+
+public class ItemModSword extends ItemSword{
+
+	public ItemModSword(String unlocalizedName, ToolMaterial material) {
+		super(material);
+		this.setUnlocalizedName(unlocalizedName);
+	}
+
+}
