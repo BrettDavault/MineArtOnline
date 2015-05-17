b285d27b0645ae6bae2d8385507095e4a3769462
gamingnut1 <brettdavault@gmail.com>
2015-05-17 11:22:24 -0500

    Create BlockMAO.java
    File to call for blocks

---
+++ BlockMAO.java
@@ -0,0 +1,13 @@
+package com.kidokenji.mineartonline.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BlockMAO extends Block{
+
+	public BlockMAO(Material materialIn) {
+		super(materialIn);
+
+	}
+
+}
